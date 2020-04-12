45a99d887193e7acf6eef8cb09faa19cdc0d6984
unknown <fiiarkhurst@gmail.com>
2020-04-12 20:17:38 +0100

    modified app.js

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Camera } from 'expo-camera';
+import { View, StyleSheet, Text } from 'react-native';
+
 
 export default function App() {
   return (
-      <Camera  />
+      <View style={styles.container}>
+        <Text style={{fontSize:19}}> {"Expo wrappers \n 1. Camera \n 2. Camera Roll \n 3. Notification"}</Text>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
 
